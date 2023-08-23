794d5c7d7acccf43a87f0ca00edd49918a0ba1cd
itsspriyansh <priyanshkp01@gmail.com>
2023-08-23 16:50:29 +0530

    swagger autogen implemented

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require('./routes/authRoutes');
 const statusRoutes = require('./routes/statusRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger_output.json');
 require('./models/index');
 require('dotenv').config();
 
@@ -11,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(authRoutes);
 app.use(statusRoutes);
